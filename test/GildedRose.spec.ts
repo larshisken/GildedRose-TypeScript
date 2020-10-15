e8bd8909c3a33209ff78d24fe8017a891858496e
Lars Hisken <lars.hisken@bettyblocks.com>
2020-10-15 09:42:10 +0200

    test: GildedRose updateItems

--- test/GildedRose.spec.ts
+++ test/GildedRose.spec.ts
@@ -1,11 +1,111 @@
 import { expect } from 'chai';
 import { GildedRose } from '../app/GildedRose';
-import { Item } from '../app/Item';
+import { createItem } from '../app/Item';
 
-describe('Gilded Rose', function () {
-  it('should foo', function () {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal('fixme');
+describe('Gilded Rose', () => {
+  it('should update items accordingly', () => {
+    const items = [
+      createItem({
+        name: '+5 Dexterity Vest',
+        sellIn: 10,
+        quality: 20,
+      }),
+      createItem({
+        name: 'Aged Brie',
+        sellIn: 2,
+        quality: 0,
+      }),
+      createItem({
+        name: 'Elixir of the Mongoose',
+        sellIn: 5,
+        quality: 7,
+      }),
+      createItem({
+        name: 'Sulfuras, Hand of Ragnaros',
+        sellIn: 3,
+        quality: 80,
+      }),
+      createItem({
+        name: 'Sulfuras, Hand of Ragnaros',
+        sellIn: -1,
+        quality: 80,
+      }),
+      createItem({
+        name: 'Backstage passes to a TAFKAL80ETC concert',
+        sellIn: 15,
+        quality: 20,
+      }),
+      createItem({
+        name: 'Backstage passes to a TAFKAL80ETC concert',
+        sellIn: 10,
+        quality: 49,
+      }),
+      createItem({
+        name: 'Backstage passes to a TAFKAL80ETC concert',
+        sellIn: 5,
+        quality: 49,
+      }),
+      // @FIXME: Handle conjured items appropriately
+      createItem({
+        name: 'Conjured Mana Cake',
+        sellIn: 3,
+        quality: 6,
+      }),
+    ];
+
+    const expected = [
+      createItem({
+        name: '+5 Dexterity Vest',
+        sellIn: 9,
+        quality: 19,
+      }),
+      createItem({
+        name: 'Aged Brie',
+        sellIn: 1,
+        quality: 1,
+      }),
+      createItem({
+        name: 'Elixir of the Mongoose',
+        sellIn: 4,
+        quality: 6,
+      }),
+      createItem({
+        name: 'Sulfuras, Hand of Ragnaros',
+        sellIn: 3,
+        quality: 80,
+      }),
+      createItem({
+        name: 'Sulfuras, Hand of Ragnaros',
+        sellIn: -1,
+        quality: 80,
+      }),
+      createItem({
+        name: 'Backstage passes to a TAFKAL80ETC concert',
+        sellIn: 14,
+        quality: 21,
+      }),
+      createItem({
+        name: 'Backstage passes to a TAFKAL80ETC concert',
+        sellIn: 9,
+        quality: 50,
+      }),
+      createItem({
+        name: 'Backstage passes to a TAFKAL80ETC concert',
+        sellIn: 4,
+        quality: 50,
+      }),
+      // @FIXME: Handle conjured items appropriately
+      createItem({
+        name: 'Conjured Mana Cake',
+        sellIn: 2,
+        quality: 4,
+      }),
+    ];
+
+    const gildedRose = new GildedRose(items);
+
+    gildedRose.updateQuality();
+
+    expect(gildedRose.items).to.deep.equals(expected);
   });
 });
