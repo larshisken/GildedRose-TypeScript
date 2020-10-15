cf778bc4aacb1bf39a6d442062334760e626eb27
Lars Hisken <lars.hisken@bettyblocks.com>
2020-10-15 09:18:14 +0200

    feat: add createItem

---
+++ app/Item.ts
@@ -0,0 +1,27 @@
+// Belongs to the goblin in the corner
+export class Item {
+  name: string;
+  sellIn: number;
+  quality: number;
+
+  constructor(name: string, sellIn: number, quality: number) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
+
+export const createItem = ({ name, quality, sellIn }: Item): Item => {
+  const minQuality = 0;
+  const maxQuality = 50;
+
+  if (quality < minQuality) {
+    throw new Error(`Quality should be higher than or equal to ${minQuality}`);
+  }
+
+  if (quality > maxQuality) {
+    throw new Error(`Quality should be lower than or equal to ${maxQuality}`);
+  }
+
+  return new Item(name, sellIn, quality);
+};
---
+++ test/Item.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import { createItem } from '../app/Item';
+
+describe('createItem', () => {
+  it('quality should never be negative', () => {
+    expect(() =>
+      createItem({ name: 'foo', sellIn: 0, quality: -1 })
+    ).to.throw();
+
+    expect(() =>
+      createItem({ name: 'foo', sellIn: 0, quality: 0 })
+    ).to.not.throw();
+  });
+
+  it('quality should be lower than or equal to 50 for normal items', () => {
+    expect(() =>
+      createItem({ name: 'foo', sellIn: 0, quality: 51 })
+    ).to.throw();
+
+    expect(() =>
+      createItem({ name: 'foo', sellIn: 0, quality: 50 })
+    ).to.not.throw();
+  });
+});
